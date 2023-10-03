8f68ffb853b77a11c3a3833a0a8e0bbdd9169cb7
DILIP SANAP <dilipinbsanap@gmail.com>
2023-10-03 11:40:45 +0530

    basic routes added

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template,jsonify
 app=Flask(__name__)
+
+jobs=[
+    {
+        'id':1,
+        'title':'Data Scientist',
+        'location':'Bengaluru, India',
+        'salary':'10,00,000'
+    },
+    {
+        'id':2,
+        'title':'Data Analyst',
+        'location':'Delhi, India',
+        'salary':'15,00,000'
+    },
+    {
+        'id':3,
+        'title':'Front End Engeeneer',
+        'location':'Bengaluru, India',
+        'salary':'12,00,000'
+    },
+    {
+        'id':4,
+        'title':'Full Stack Developer',
+        'location':'Remote',
+        'salary':'25,00,000'
+    }
+]
 
 @app.route("/")
 def hello_world():
-    return "Hello, world"
+    return render_template('home.html')
 
+@app.route('/api/jobs')
+def jobs_function():
+    return jsonify(jobs)
 
 if __name__=="__main__":
     app.run(host='0.0.0.0',debug=True)
